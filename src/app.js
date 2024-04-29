cc3ef76a31158c490a3c54e1148962f912080bda
Isaias-tech <isaias-santiago-toribio@outlook.com>
2024-04-29 15:18:42 -0400

    Feature: Add gzip compression to app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const morgan = require("morgan");
 const cors = require("cors");
+const compression = require("compression");
 
 const express = require("express");
 const app = express();
 
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
